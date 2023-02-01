70a62282bd3bd83c45e46eace917f1c6daa83408
Calgor <ahomayoni684@gmail.com>
2023-02-01 09:48:32 +0330

    fixed the bug with Recursive bfs

--- GraphAlgoritgms/DFS/main.go
+++ GraphAlgoritgms/DFS/main.go
@@ -3,38 +3,37 @@ package main
 import "fmt"
 
 var time int
+var visited, parent, t1, t2 [5]int
 
 func main() {
 	graph := make([][]int, 5)
 	graph[0] = []int{1, 3}
-	graph[1] = []int{1, 3, 4}
+	graph[1] = []int{0, 3, 4}
 	graph[2] = []int{0, 3}
 	graph[3] = []int{1, 2, 4}
 	graph[4] = []int{1, 3}
-	var visited, parent, t1, t2 [5]int
-	dfs(graph, visited, parent, t1, t2)
+	fmt.Printf("0 ")
+	dfs(graph)
 
 }
 
-func dfs(graph [][]int, visited, parent, t1, t2 [5]int) {
+func dfs(graph [][]int) {
 	for k := range graph {
 		if visited[k] == 0 {
-			dfsVisit(graph, visited, parent, t1, t2, k)
+			dfsVisit(graph, k)
 		}
 	}
 }
 
-func dfsVisit(graph [][]int, visited, parent, t1, t2 [5]int, root int) {
+func dfsVisit(graph [][]int, root int) {
 	time++
 	t1[root] = time
 	visited[root] = 1
-	for k2, v2 := range graph[root] {
+	for _, v2 := range graph[root] {
 		if visited[v2] == 0 {
-			fmt.Println(graph[root][k2])
-			visited[v2] = 1
+			fmt.Printf("%v ", v2)
 			parent[v2] = root
-			dfsVisit(graph, visited, parent, t1, t2, root)
-
+			dfsVisit(graph, v2)
 		}
 	}
 	visited[root] = 2

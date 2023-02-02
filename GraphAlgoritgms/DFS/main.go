93ee47e6f745fa84f6a3ad0005cb6b1335444575
Calgor <ahomayoni684@gmail.com>
2023-02-02 22:06:00 +0330

    added non recursive DFS but they are different

--- GraphAlgoritgms/DFS/main.go
+++ GraphAlgoritgms/DFS/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-var time int
-var visited, parent, t1, t2 [5]int
+	Stack "github.com/Calgorr/Data-Structures-and-Algorithms-with-Go/ElementaryDataStructures/stack/stack"
+)
+
+var stack *Stack.Stack[int]
+var visited [5]int
 
 func main() {
 	graph := make([][]int, 5)
@@ -12,32 +16,22 @@ func main() {
 	graph[2] = []int{0, 3}
 	graph[3] = []int{1, 2, 4}
 	graph[4] = []int{1, 3}
-	fmt.Printf("0 ")
-	dfs(graph)
-
+	stack = Stack.NewStack[int]()
+	DFS(graph, 0)
 }
 
-func dfs(graph [][]int) {
-	for k := range graph {
-		if visited[k] == 0 {
-			dfsVisit(graph, k)
-		}
-	}
-}
-
-func dfsVisit(graph [][]int, root int) {
-	time++
-	t1[root] = time
+func DFS(graph [][]int, root int) {
+	stack.Push(root)
 	visited[root] = 1
-	for _, v2 := range graph[root] {
-		if visited[v2] == 0 {
-			fmt.Printf("%v ", v2)
-			parent[v2] = root
-			dfsVisit(graph, v2)
-
+	for !stack.IsEmpty() {
+		v1, _ := stack.Pop()
+		fmt.Print(v1, " ")
+		for _, v2 := range graph[v1] {
+			if visited[v2] != 1 {
+				visited[v2] = 1
+				stack.Push(v2)
+			}
 		}
 	}
-	visited[root] = 2
-	time++
-	t2[root] = time
+	fmt.Println()
 }

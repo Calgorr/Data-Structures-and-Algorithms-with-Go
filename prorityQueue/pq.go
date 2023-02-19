ea5287ee7daebbbc16b8e62bae5bb1bd1166bc15
Calgor <ahomayoni684@gmail.com>
2023-02-19 17:54:02 +0330

    added another func

--- prorityQueue/pq.go
+++ prorityQueue/pq.go
@@ -1,5 +1,24 @@
 package prorityqueue
 
-func HeapMax(heap []int) int {
+import (
+	Heap "github.com/Calgorr/Data-Structures-and-Algorithms-with-Go/Heap"
+)
+
+type Number interface {
+	int | interface{}
+}
+
+func HeapMax(heap []int) Number {
 	return heap[0]
+}
+
+func HeapExtractMax(heap []int) Number {
+	if len(heap) < 1 {
+		return nil
+	}
+	max := HeapMax(heap)
+	heap[0] = heap[len(heap)-1]
+	heap = heap[1:]
+	Heap.MaxHeapify(heap, len(heap), 0)
+	return max
 }
